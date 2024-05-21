46e3ecde2f630bf0fe6e18a709e6d59bba859c85
wcasarino <wcasarino@gmail.com>
2024-05-21 15:15:20 -0300

    21-05-24

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,20 @@
+'use client'; 
+
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}: {amount: number}) => {
+  return (
+    <div className='w-full'>
+        <CountUp 
+            decimal='.'
+            prefix='$'
+            decimals={2}
+            // duration={2.75}
+            end={amount}
+        />
+    </div>
+  )
+}
+
+
+export default AnimatedCounter
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,11 +1,12 @@
 import { formatAmount } from '@/lib/utils'
-import React from 'react'
+import AnimatedCounter from './AnimatedCounter'
+import DoughnutChart from './DoughnutChart'
 
 const TotalBalanceBox = ({ accounts=[], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
   return (
     <section className='total-balance'>
         <div className="total-balance-chart">
-            {/* DoughnutChart */}
+            <DoughnutChart accounts={accounts}/>
         </div>
 
         <div className="flex flex-col gap-6">
@@ -16,9 +17,10 @@ const TotalBalanceBox = ({ accounts=[], totalBanks, totalCurrentBalance }: Totla
                 <p className='total-balance-label'>
                     Total Current Balance
                 </p>
-                <p className='total-balabnce-amount flex-center gap-2'>
-                    {formatAmount(totalCurrentBalance)}
-                </p>
+                <div className='total-balabnce-amount flex-center gap-2'>
+                    <AnimatedCounter amount = {totalCurrentBalance}/>
+                    {/* {formatAmount(totalCurrentBalance)} */}
+                </div>
             </div>
         </div>
 
